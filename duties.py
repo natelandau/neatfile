97188e8ac85902d8b9816a3acadb585d2b6d9933
Nathaniel Landau <nate@natelandau.com>
2025-08-06 16:26:40 -0400

    ci: improve workflows

--- duties.py
+++ duties.py
@@ -211,6 +211,7 @@ def lint(ctx: Context) -> None:
 def update(ctx: Context) -> None:
     """Update the project."""
     ctx.run(["uv", "lock", "--upgrade"], title="update uv lock")
+    ctx.run(["uv", "sync"], title="update uv dependencies")
     ctx.run(["pre-commit", "autoupdate"], title="pre-commit autoupdate")
 
 
